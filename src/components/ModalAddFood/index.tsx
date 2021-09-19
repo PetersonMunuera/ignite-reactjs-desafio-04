16251120c8847ac37e7d4357502ba35d2eacc0d5
Peterson Munuera <peterson.munuera@gmail.com>
2021-09-19 20:01:34 -0300

    arquivos refatorados de js para ts e classes transformadas em functions

---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,50 @@
+import {  createRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import { Modal } from '../Modal';
+import Input from '../Input';
+import { FormHandles } from '@unform/core';
+
+type FoodProps = {
+  name: string
+  description: string
+  price: number
+  available: boolean
+  image: string
+}
+
+type ModalAddFoodProps = {
+  isOpen: boolean
+  setIsOpen: () => void
+  handleAddFood: (food: FoodProps) => void
+}
+
+export function ModalAddFood(props: ModalAddFoodProps) {
+  const formRef = createRef<FormHandles>();
+
+  async function handleSubmit(data: FoodProps) {
+    props.handleAddFood(data);
+    props.setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={props.isOpen} setIsOpen={props.setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
